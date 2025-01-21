b9aae3f875702e3fe0173faca54ecbd7ab971310
AmazinAxel <axel@amazinaxel.com>
2025-01-20 21:01:21 -0800

    Rewrite

--- README.md
+++ README.md
@@ -1,24 +1,11 @@
-# 📒 Discord Message Formatter
+# Discord Message Formatter
 
-This is a simple Discord message formatter that uses embeds to format messages in a more appealing way. It uses the Discord Webhook API to send messages and is easily customizable.
+Simple Discord embed creator using Workers. Features an extensible Turnstile-supported password-protected form as a frontend.
 
-## ❓ How to Use
+## Setup
 
-Clone the repo, create a Cloudflare account, and run `bun i` to install deps. Rename the `wrangler.toml.example` file to `wrangler.toml` and modify it to fit your needs. Then, run `bun run deploy` to deploy. Then, log in with your Cloudflare account.
+- Run `bun i` to install deps.
+- Rename the example `wrangler.toml.example` to `wrangler.toml` and modify it accordingly.
+- Run `bun run deploy` to deploy the Worker. Then, log in with your Cloudflare account.
 
-**TURNSTILE:** You can add a Turnstile form for bot spam and brute force protection. Go to the CF Dashboard, add a site, and copy the keys into the `wrangler.toml` file.
-
-*Note:* you can replace `bun` with `npm` to use on npm
-
-## 🧑‍💻 Local Development
-
-Run `bun run dev` to start the Wrangler dev server. To pass Turnstile, paste in these developer keys:
-
-```
-TURNSTILE_SITEKEY = "1x00000000000000000000AA"
-TURNSTILE_SECRET = "1x0000000000000000000000000000000AA"
-```
-
-## 🌄 What it looks like
-
-![Discord message formatter image preview](format-bot-preview.png)
+Run `bun run dev` to start the Wrangler dev server for fast development.
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,28 @@
 interface Env {
-  USERNAME: string;
   WEBHOOK_URL: string;
-  PROJECT_NAME: string;
   PASSWORD: string;
-  TITLE_PLACEHOLDER: string;
-  PRETITLE_PLACEHOLDER: string;
   EMBED_COLOR: string;
   TURNSTILE_SITEKEY: string;
   TURNSTILE_SECRET: string
 }
 
 interface formData extends FormData {
-  preTitle: string;
+  author: string;
   title: string;
   message: string;
   footer: string;
   password: string;
 }
 
-// TODO: add support for multiple fields
-const format = (preTitle: string, title: string, message: string, footer: string, env: Env) => {
+// TODO: add support for multiple embeds
+const format = (author: string, title: string, message: string, footer: string, env: Env) => {
   return {
     method: "POST",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify({
-      "username": env.USERNAME,
       "embeds": [{
         "footer": { "text": footer },
-        "author": { "name": preTitle },
+        "author": { "name": author },
         "title": title,
         "description": message,
         "color": env.EMBED_COLOR,
@@ -38,54 +33,45 @@ const format = (preTitle: string, title: string, message: string, footer: string
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
+    
+    // Handle formdata here
     if (request.method.toUpperCase() === "POST") {
-      const data = await request.formData() as formData; // Get all form data
+      const data = await request.formData() as formData;
       
-      /***      Run a few checks      ***/
-
       // Check if the user passed the Turnstile form
-      const token = data.get("cf-turnstile-response") as string; // Get Turnstile token
-      const ip = request.headers.get("CF-Connecting-IP") as string; // Get IP
+      const token = data.get("cf-turnstile-response") as string;
+      const ip = request.headers.get("CF-Connecting-IP") as string;
+      const formData = new FormData();
+      formData.append("secret", env.TURNSTILE_SECRET);
+      formData.append("response", token);
+      formData.append("remoteip", ip);
     
-      // Verify Turnstile token
-      let formData = new FormData() // Create FormData
-      formData.append("secret", env.TURNSTILE_SECRET); // Get private key secret from encrypted environment variable
-      formData.append("response", token); // Get the response token
-      formData.append("remoteip", ip); // Pass the user's IP
-    
-      // Get response from Turnstile API
       const turnstileResponse = await fetch("https://challenges.cloudflare.com/turnstile/v0/siteverify", { body: formData, method: "POST"});
-    
       const turnstileStatus = await turnstileResponse.json() as { success: boolean }; // Get Turnstile report
     
-      // Check if the user didn't pass the captcha
-      if (!turnstileStatus.success) { return new Response('Captcha verification failed. Please try again.', { status: 511, headers: new Headers({"Access-Control-Allow-Origin": "*"}) });}
+      if (!turnstileStatus.success) // User did not pass the captcha
+        return new Response('Captcha verification failed', { status: 511, headers: new Headers({"Access-Control-Allow-Origin": "*"}) });
 
-      if (data.get("message") == '') // Check if a message was given
-        return new Response("Please enter a message!", { status: 400 });
-
-      try { new URL(env.WEBHOOK_URL); } // Check if webhook URL is valid
-      catch (err) { return new Response("Invalid webhook URL!", { status: 500 }); }
+      if (data.get("message") == '') // Message was not given
+        return new Response("Please enter a message", { status: 400 });
     
-      if (data.get("password") != env.PASSWORD) // Check if password is correct
-        return new Response("Incorrect password!", { status: 401 });
+      if (data.get("password") != env.PASSWORD) // Password is not correct
+        return new Response("Incorrect password", { status: 401 });
 
-      // Finally, send the data to the webhook
-      await fetch(env.WEBHOOK_URL, format(data.get("preTitle") as string, data.get("title") as string, data.get("message") as string, data.get("footer") as string, env));
+      // Passed all checks - submit the embed
+      await fetch(env.WEBHOOK_URL, format(data.get("author") as string, data.get("title") as string, data.get("message") as string, data.get("footer") as string, env));
       return new Response("Sent Successfully!", { status: 200 });
     };
-    
 
     // Return the webpage
-    return new Response(`<!DOCTYPE html>
+    return new Response(`
+<!DOCTYPE html>
 <html lang="en-US">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <meta name="description" content="My place on the Internet to share things about me, what I'm working on, projects, and more!">
-  <title>${env.PROJECT_NAME} Discord Format Bot</title>
+  <title>Discord Embed Creator</title>
   <meta name="color-scheme" content="dark light">
-  <link rel="stylesheet" href="https://alecshome.com/style.css">
   <script defer src="https://challenges.cloudflare.com/turnstile/v0/api.js"></script>
 
   <!-- SimpleMDE -->
@@ -97,29 +83,27 @@ export default {
   </style>
 </head>
   <body>
-    <div class="card">
-      <h1>${env.PROJECT_NAME} Discord Format Bot</h1>
-      <form id="form" method="post" enctype="multipart/form-data">
-        <label for="preTitle">Pretitle:</label><br>
-        <input type="text" id="preTitle" name="preTitle" placeholder="${env.PRETITLE_PLACEHOLDER}"><br>
+    <h1>Discord Embed Creator</h1>
+    <form id="form" method="post" enctype="multipart/form-data">
+      <label for="author">Author:</label><br>
+      <input type="text" id="author" name="author" placeholder="Author placeholder"><br>
 
-        <label for="title">Title:</label><br>
-        <input type="text" id="title" name="title" placeholder="${env.TITLE_PLACEHOLDER}"><br>
+      <label for="title">Title:</label><br>
+      <input type="text" id="title" name="title" placeholder="Title placeholder"><br>
 
-        <label for="message">Message:</label><br><br>
-        <textarea rows="10" cols="50" id="message" name="message"></textarea><br>
+      <label for="message">Message:</label><br><br>
+      <textarea rows="10" cols="50" id="message" name="message"></textarea><br>
 
-        <label for="footer">Footer:</label><br>
-        <textarea rows="1" id="footer" name="footer"></textarea><br>
+      <label for="footer">Footer:</label><br>
+      <textarea rows="1" id="footer" name="footer"></textarea><br>
 
-        <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password">
-        
-        <div class="cf-turnstile" data-sitekey="${env.TURNSTILE_SITEKEY}" data-theme="auto" id="turnstileWidget"></div>
+      <label for="password">Password:</label><br>
+      <input type="password" id="password" name="password">
+      
+      <div class="cf-turnstile" data-sitekey="${env.TURNSTILE_SITEKEY}" data-theme="auto" id="turnstileWidget"></div>
 
-        <input type="submit" class="button" value="Send Announcement">
-      </form>
-    </div>
+      <input type="submit" value="Send Announcement">
+    </form>
   </body>
   <script>new SimpleMDE({autoDownloadFontAwesome: true, forceSync: true, status: false});</script>
 </html>`, { status: 200, headers: { "Content-Type": "text/html; charset=utf-8" }});
